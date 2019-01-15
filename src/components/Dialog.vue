7c610f977eda1dc91e87dcf8e563ca7e6029efae
ganp1020 <ganp1020@outlook.com>
2019-01-15 12:10:41 +0800

    代码规范修改

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="dialog-container">
+    <div class="dialog__container">
         <div class="dialog__wrapper" v-if="visible" @click="closeModal">
-            <div class="dialog">
+            <div class="dialog__content">
                 <div class="dialog__header">
                     <div class="dialog__title">{{ title }}</div>
                 </div>
@@ -31,41 +31,44 @@
                 this.$emit('update:visible', false) // 传递关闭事件
             },
             closeModal(e) {
+                console.log(e)
                 if (this.visible) {
-                    document.querySelector('.dialog').contains(e.target) ? '' : this.close(); // 判断点击的落点在不在dialog对话框内，如果在对话框外就调用this.close()方法关闭对话框
+                    document.querySelector('.dialog__content').contains(e.target) ? '' : this.close(); // 判断点击的落点在不在dialog对话框内，如果在对话框外就调用this.close()方法关闭对话框
                 }
             }
         }
     }
 </script>
 <style lang="scss" scoped>
-    .dialog__wrapper{
-        z-index: 2001;
-        position: fixed;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        overflow: auto;
-        margin: 0;
-        .dialog{
-            position: relative;
-            margin: 0 auto 50px;
-            background: #fff;
-            border-radius: 2px;
-            box-shadow: 0 1px 3px rgba(0,0,0,.3);
-            box-sizing: border-box;
-            width: 50%;
+    .dialog__container {
+        .dialog__wrapper {
+            z-index: 2001;
+            position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            overflow: auto;
+            margin: 0;
+            .dialog__content {
+                position: relative;
+                margin: 0 auto 50px;
+                background: #fff;
+                border-radius: 2px;
+                box-shadow: 0 1px 3px rgba(0, 0, 0, .3);
+                box-sizing: border-box;
+                width: 50%;
+            }
         }
-    }
-    .modal{
-        z-index: 2000;
-        position: fixed;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        opacity: .5;
-        background: #000;
+        .modal {
+            z-index: 2000;
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            opacity: .5;
+            background: #000;
+        }
     }
 </style>
